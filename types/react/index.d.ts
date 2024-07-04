8991bbd2ceb4e788e70604e1602a37403ba43923
Louis Cruz <louisstephancruz@me.com>
2024-07-04 12:00:48 -0700

    Add MathMLMpaddedElement

--- types/react/index.d.ts
+++ types/react/index.d.ts
@@ -3778,6 +3778,13 @@ declare namespace React {
     interface MathMLMoverElement extends MathMLAttributes<MathMLMoverElement> {
         accent?: boolean | undefined;
     }
+    interface MathMLMpaddedElement extends MathMLAttributes<MathMLMpaddedElement> {
+        depth?: string | undefined;
+        height?: string | undefined;
+        lspace?: string | undefined;
+        voffset?: string | undefined;
+        width?: string | undefined;
+    }
 
     interface WebViewHTMLAttributes<T> extends HTMLAttributes<T> {
         allowFullScreen?: boolean | undefined;
@@ -4246,6 +4253,7 @@ declare namespace React {
             mn: React.MathMLProps<MathMLMnElement>;
             mo: React.MathMLProps<MathMLMoElement>;
             mover: React.MathMLProps<MathMLMoverElement>;
+            mpadded: React.MathMLProps<MathMLMpaddedElement>;
         }
     }
 }
