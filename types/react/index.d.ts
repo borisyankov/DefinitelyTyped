a896569a399373fcfa1571dd07e9e471fc7a7512
Louis Cruz <louisstephancruz@me.com>
2024-07-04 13:23:12 -0700

    Add annotation elements

--- types/react/index.d.ts
+++ types/react/index.d.ts
@@ -3848,7 +3848,22 @@ declare namespace React {
         accent?: boolean | undefined;
         accentunder?: boolean | undefined;
     }
+    /**
+     * @see https://w3c.github.io/mathml-core/#semantics-and-presentation
+    */
     interface MathMLSemanticsElement extends MathMLAttributes<MathMLSemanticsElement> {}
+    /**
+     * @see https://w3c.github.io/mathml-core/#semantics-and-presentation
+    */
+    interface MathMLAnnotationElement extends MathMLAttributes<MathMLAnnotationElement> {
+        encoding?: string | undefined;
+    }
+    /**
+     * @see https://w3c.github.io/mathml-core/#semantics-and-presentation
+    */
+    interface MathMLAnnotationXmlElement extends MathMLAttributes<MathMLAnnotationXmlElement> {
+        encoding?: string | undefined;
+    }
 
     interface WebViewHTMLAttributes<T> extends HTMLAttributes<T> {
         allowFullScreen?: boolean | undefined;
@@ -4334,6 +4349,10 @@ declare namespace React {
             mtr: React.MathMLProps<MathMLMtrElement>;
             munder: React.MathMLProps<MathMLMunderElement>;
             munderover: React.MathMLProps<MathMLMunderoverElement>;
+            semantics: React.MathMLProps<MathMLSemanticsElement>;
+            // MathML semantic annotations
+            annotation: React.MathMLProps<MathMLAnnotationElement>;
+            'annotation-xml': React.MathMLProps<MathMLAnnotationXmlElement>;
         }
     }
 }
